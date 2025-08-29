2242e946f02dc4ebbf1ffb9698615321a0b478ea
Bradley <bradleywong99@gmail.com>
2025-08-28 22:35:31 -0700

    Tailwind update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
-  return <h1>Home Page!</h1>;
+export default function Welcome() {
+  return (
+    <h1 className="font-bold text-teal-500 text-2xl">Welcome Page!</h1>
+  )
 }
+
